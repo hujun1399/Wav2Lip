bca314554dee4bfb1ad9c8a4a30e4e8e0d31c88a
hujun1399 <hujun1399@163.com>
2023-07-07 11:37:24 +0800

    reformat code

--- color_syncnet_train.py
+++ color_syncnet_train.py
@@ -13,15 +13,22 @@ import numpy as np
 
 from glob import glob
 
-import os, random, cv2, argparse
+import os
+import random
+import cv2
+import argparse
 from hparams import hparams, get_image_list
 
-parser = argparse.ArgumentParser(description='Code to train the expert lip-sync discriminator')
+parser = argparse.ArgumentParser(
+    description='Code to train the expert lip-sync discriminator')
 
-parser.add_argument("--data_root", help="Root folder of the preprocessed LRS2 dataset", required=True)
+parser.add_argument(
+    "--data_root", help="Root folder of the preprocessed LRS2 dataset", required=True)
 
-parser.add_argument('--checkpoint_dir', help='Save checkpoints to this directory', required=True, type=str)
-parser.add_argument('--checkpoint_path', help='Resumed from this checkpoint', default=None, type=str)
+parser.add_argument('--checkpoint_dir',
+                    help='Save checkpoints to this directory', required=True, type=str)
+parser.add_argument('--checkpoint_path',
+                    help='Resumed from this checkpoint', default=None, type=str)
 
 args = parser.parse_args()
 
@@ -33,6 +40,7 @@ print('use_cuda: {}'.format(use_cuda))
 
 syncnet_T = 5
 syncnet_mel_step_size = 16
+
 
 class Dataset(object):
     def __init__(self, split):
@@ -60,8 +68,7 @@ class Dataset(object):
 
         end_idx = start_idx + syncnet_mel_step_size
 
-        return spec[start_idx : end_idx, :]
-
+        return spec[start_idx: end_idx, :]
 
     def __len__(self):
         return len(self.all_videos)
@@ -105,7 +112,8 @@ class Dataset(object):
 
                 window.append(img)
 
-            if not all_read: continue
+            if not all_read:
+                continue
 
             try:
                 wavpath = join(vidname, "audio.wav")
@@ -130,19 +138,23 @@ class Dataset(object):
 
             return x, mel, y
 
+
 logloss = nn.BCELoss()
+
+
 def cosine_loss(a, v, y):
     d = nn.functional.cosine_similarity(a, v)
     loss = logloss(d.unsqueeze(1), y)
 
     return loss
 
+
 def train(device, model, train_data_loader, test_data_loader, optimizer,
           checkpoint_dir=None, checkpoint_interval=None, nepochs=None):
 
     global global_step, global_epoch
     resumed_step = global_step
-    
+
     while global_epoch < nepochs:
         running_loss = 0.
         prog_bar = tqdm(enumerate(train_data_loader))
@@ -166,17 +178,22 @@ def train(device, model, train_data_loader, test_data_loader, optimizer,
             cur_session_steps = global_step - resumed_step
             running_loss += loss.item()
 
+            # print("global_epoch={}, global_step={}, cur_loss={}".format(global_epoch, global_step, loss.item()))
+
             if global_step == 1 or global_step % checkpoint_interval == 0:
                 save_checkpoint(
                     model, optimizer, global_step, checkpoint_dir, global_epoch)
 
             if global_step % hparams.syncnet_eval_interval == 0:
                 with torch.no_grad():
-                    eval_model(test_data_loader, global_step, device, model, checkpoint_dir)
+                    eval_model(test_data_loader, global_step,
+                               device, model, checkpoint_dir)
 
-            prog_bar.set_description('Loss: {}'.format(running_loss / (step + 1)))
+            prog_bar.set_description('Loss: {}, global_epoch={}, global_step={}, cur_loss={}'.format(
+                running_loss / (step + 1), global_epoch, global_step, loss.item()))
 
         global_epoch += 1
+
 
 def eval_model(test_data_loader, global_step, device, model, checkpoint_dir):
     eval_steps = 1400
@@ -198,12 +215,14 @@ def eval_model(test_data_loader, global_step, device, model, checkpoint_dir):
             loss = cosine_loss(a, v, y)
             losses.append(loss.item())
 
-            if step > eval_steps: break
+            if step > eval_steps:
+                break
 
         averaged_loss = sum(losses) / len(losses)
         print(averaged_loss)
 
         return
+
 
 def save_checkpoint(model, optimizer, step, checkpoint_dir, epoch):
 
@@ -218,6 +237,7 @@ def save_checkpoint(model, optimizer, step, checkpoint_dir, epoch):
     }, checkpoint_path)
     print("Saved checkpoint:", checkpoint_path)
 
+
 def _load(checkpoint_path):
     if use_cuda:
         checkpoint = torch.load(checkpoint_path)
@@ -225,6 +245,7 @@ def _load(checkpoint_path):
         checkpoint = torch.load(checkpoint_path,
                                 map_location=lambda storage, loc: storage)
     return checkpoint
+
 
 def load_checkpoint(path, model, optimizer, reset_optimizer=False):
     global global_step
@@ -243,11 +264,13 @@ def load_checkpoint(path, model, optimizer, reset_optimizer=False):
 
     return model
 
+
 if __name__ == "__main__":
     checkpoint_dir = args.checkpoint_dir
     checkpoint_path = args.checkpoint_path
 
-    if not os.path.exists(checkpoint_dir): os.mkdir(checkpoint_dir)
+    if not os.path.exists(checkpoint_dir):
+        os.mkdir(checkpoint_dir)
 
     # Dataset and Dataloader setup
     train_dataset = Dataset('train')
@@ -265,13 +288,15 @@ if __name__ == "__main__":
 
     # Model
     model = SyncNet().to(device)
-    print('total trainable params {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+    print('total trainable params {}'.format(sum(p.numel()
+          for p in model.parameters() if p.requires_grad)))
 
     optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad],
                            lr=hparams.syncnet_lr)
 
     if checkpoint_path is not None:
-        load_checkpoint(checkpoint_path, model, optimizer, reset_optimizer=False)
+        load_checkpoint(checkpoint_path, model,
+                        optimizer, reset_optimizer=False)
 
     train(device, model, train_data_loader, test_data_loader, optimizer,
           checkpoint_dir=checkpoint_dir,
